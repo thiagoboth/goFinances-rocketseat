fb252073dd77b360aca5b4eeeb71b3562fa9929c
thiagoboth <thiagoboth@hotmail.com>
2022-02-28 13:33:57 -0300

    transformando arquivos js em ts / cores e fontes globais com styled-components e expo-google-fonts

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,9 @@
+//sobrescrevendo tipos
+import 'styled-components';
+import theme from './theme';
+
+declare module 'styled-components' {
+    type ThemeType = typeof theme
+
+    export interface DefaultTheme extends ThemeType {}
+}
