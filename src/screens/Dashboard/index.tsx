185d6b805359d5c5812d5b13cafe8a728bbe1c47
thiagoboth <thiagoboth@hotmail.com>
2022-02-28 14:12:44 -0300

    feat: userInfos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -3,12 +3,29 @@ import { Text } from 'react-native';
 import {
     Container,
     Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
 } from './styles';
 
 export function Dashboard() {
     return (
         <Container>
             <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/82188895?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Thiago</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
                 
             </Header>
 
